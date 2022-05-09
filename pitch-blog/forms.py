efaf69ed8aca793cf7555e8e8be509797a10933d
Liz <adhiamboliz3@gmail.com>
2022-05-09 10:53:51 +0300

    Added login page

--- pitch-blog/forms.py
+++ pitch-blog/forms.py
@@ -11,3 +11,10 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    submit = SubmitField('Login') 
