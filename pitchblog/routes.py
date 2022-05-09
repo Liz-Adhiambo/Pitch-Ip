946a4fa72b5a1d4d033ffd3edb3742e903aa1c1c
Liz <adhiamboliz3@gmail.com>
2022-05-09 18:14:16 +0300

    Added create post file

--- pitchblog/routes.py
+++ pitchblog/routes.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 from flask import render_template, url_for, flash, redirect,request
 from pitchblog import app,db, bcrypt
-from pitchblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from pitchblog.forms import RegistrationForm, LoginForm, UpdateAccountForm,PostForm
 from pitchblog.models import User, Post
 from flask_login import login_user,logout_user, current_user, login_required
 
@@ -104,3 +104,10 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+    
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+   
+    return render_template('create_post.html', title='New Post',
+                           )
