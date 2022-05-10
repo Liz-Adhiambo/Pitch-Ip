44635d11815998a4a3407695b98f5171ca8086d0
Liz <adhiamboliz3@gmail.com>
2022-05-10 16:30:21 +0300

    Changed delete message

--- pitchblog/posts/routes.py
+++ pitchblog/posts/routes.py
@@ -56,5 +56,5 @@ def delete_post(post_id):
         abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been deleted!', 'success')
+    flash('Your post has been deleted Successfuly!', 'success')
     return redirect(url_for('main.home'))
